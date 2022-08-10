3900940236ea8846e8964a11b397ec670fe0c59b
Flor098 <107156790+Flor098@users.noreply.github.com>
2022-08-10 13:53:50 -0300

    resueltos

--- ejercicios.js
+++ ejercicios.js
@@ -1,35 +1,69 @@
-function crearUsuario() {
+/*function Usuario(opciones){
+  this.usuario = opciones.usuario;
+  this.nombre = opciones.nombre;
+  this.email = opciones.email;
+  this.password = opciones.password;
+  Usuario.prototype.saludar = function() {
+    return 'Hola, mi nombre es' + this.nombre
+  }
+}
     // Crea una función constructor llamada "Usuario"
     // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
     // En el `contructor`, define el usuario, el nombre, el email y la contraseña
     // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
     // {{nombre}} debe ser el nombre definido en cada instancia
     // Devuelve la clase
-    // Tu código:
-  
-  }
+    // Tu código:const Usuario = crearUsuario();
+
+var user= new Usuario({usuario: 'Flor', nombre: 'Florencia', email: 'flor_09@hotmail.com', password:'Flo1234'});
+    console.log(user);
+  */
 
 
-
-  function creaObj() { 
+/*
+function datosPersona(nombre,apellido,dni){
+  this.nombre=nombre;
+  this.apellido=apellido;
+  this.dni=dni;
+}
+ */ /*function creaObj() { 
     // Crea una función constructor llamada "datosPersona" y luego crea dos personas nuevas con datos de datosPersona
     // Los datos, son nombre, apellido y dni
     // Tu código:
-  }
-  
-  
-  function creaObj2() { 
+     
+  }*/
+  /*const persona1= new datosPersona({nombre:"Mario", apellido:"Paez", dni:"3456243"})
+    const persona2=new datosPersona({nombre:"Lucas", apellido:"Gomez",dni:"3456243"})
+    console.log(persona1);
+    console.log(persona2);
+  */
+ 
+  /*function creaObj2() { 
     // Crea una función constructor llamada "Default" y luego crea dos personas nuevas con datos de datosPersona sin pasarle ningun campo
     // Los datos, son nombre, apellido y dni
     // Los datos que deberian mostrarse al acceder a uno de ellos deben ser por deafult, los que ustedes quieran
     // Tu código:
-  }
+  }*/
   
-  function Computador() { 
+  /*function Computador(id,marca,procesador,ram,ssd) { 
+    this.id=id;   
+    Computador.id = function() {
+      return 'Id:' + this.id;
+    }
+    this.marca=marca;
+    this.procesador=procesador;
+    this.ram=ram;
+    this.ssd=ssd;
+    var datos= function(){
+      return this.id,this.marca,this.procesador,this.ram,this.ssd;
+    }
+  }
+  const pc=new Computador('12345','acer','intel','8Gb','12234')
+  console.log(pc)
     // Deben crear una funcion constructora
     // Los datos son, id, marca, procesador, ram, ssd.
     // Deben simular el inicio de una pc en donde primero muestre el mensaje que esta iniciando y luego una funcion que me diga los datos de la computadora.
     // La funcion debe estar echa dentro de la funcion constructora.
     // Los datos de la computadora deben guardarse dentro de otra variable y mostrar esa persona para ver los datos.
-    // Tu código:
-  }
+    */
+  
